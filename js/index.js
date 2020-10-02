13b02ebbab6d7f4aeaaba5345c05ad6029a43899
shan-shaji <shanshaji224@gmail.com>
2020-10-02 19:35:07 +0530

    ui: Updated ui:lipstick: in page and added new button page:sparkles:

--- js/index.js
+++ js/index.js
@@ -1,4 +1,34 @@
+// animation part
 const card = document.querySelectorAll(".card");
 card.forEach((card) => {
   card.classList.add("magictime", "swashIn");
+});
+
+// color part
+// Random color creation
+var color = [
+  "#56D1D8",
+  "#0078D7",
+  "#EF584A",
+  "#FF5864",
+  "#ab0e86",
+  "#e01171",
+  "#2aaf74",
+  "#4ed99c",
+  "#f7aa00",
+  "#303841",
+];
+
+const mainPageCards = document.querySelectorAll(".main-page");
+mainPageCards.forEach((mainPageCard) => {
+  let selectedColor = color[Math.floor(Math.random() * color.length)];
+  mainPageCard.style.backgroundColor = selectedColor;
+});
+
+// Random emoji creation
+var emojisList = ["😃", "😁", "🤣", "🙃", "🥰", "😝", "🤔", "😐"];
+const emojis = document.querySelectorAll(".emoji");
+emojis.forEach((emoji) => {
+  let selectedEmoji = emojisList[Math.floor(Math.random() * emojis.length)];
+  emoji.textContent = selectedEmoji;
 });
